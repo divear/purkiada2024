1aece99ca2ce337bbe8548e3b19a9545f99884cf
lukascobit <lukyn.odehnal333@gmail.com>
2023-09-22 19:37:02 +0200

    programovaci textarea

--- pages/hra.tsx
+++ pages/hra.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Hra() {
+    const [codeVal, setCodeVal] = useState("")
+    useEffect(() => {
+        setCodeVal(`for (i = 1; i <= n; i++) {
+    if (i % 15 == 0)
+        printf("fizzbuzz");\n
+    else if (i % 5 == 0)
+        printf("buzz");\n
+    else if (i % 3 == 0)
+        printf("fizz");\n
+    else
+        printf("%d", i);\n
+}
+`)
+    }, [])
+
     return (
         <div>
             <h1>Pomoz zpravit syntax errory po rekrutovi.</h1>
             <p>Protip: Compiler má vždy pravdu</p>
+            <textarea onChange={e => setCodeVal(e.target.value)} spellCheck={false} value={codeVal} name="" id="" cols={50} rows={15}>
+
+            </textarea>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,11 @@ export default function Home() {
 			<div className="intro">
 				<h1>Purkiada bug hunt</h1>
 				<p>Úloha číslo 4 nebo kolik idk</p>
-				<button onClick={() => window.location.href = "/hra"}>start</button>
+				<form action="/hra/">
+					<input type="text" placeholder="Jméno" />
+					<input type="text" placeholder="Heslo" />
+					<button onClick={() => window.location.href = "/hra"}>start</button>
+				</form>
 			</div>
 		</div >
 	);
