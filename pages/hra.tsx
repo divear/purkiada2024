911770d67e731713139e499f04f269d8dc701b20
lukascobit <lukyn.odehnal333@gmail.com>
2023-09-23 22:20:32 +0200

    error log styling

--- pages/hra.tsx
+++ pages/hra.tsx
@@ -3,26 +3,39 @@ import React, { useEffect, useState } from 'react'
 function Hra() {
     const [codeVal, setCodeVal] = useState("")
     useEffect(() => {
-        setCodeVal(`for (i = 1; i <= n; i++) {
-    if (i % 15 == 0)
-        printf("fizzbuzz");\n
-    else if (i % 5 == 0)
-        printf("buzz");\n
-    else if (i % 3 == 0)
-        printf("fizz");\n
-    else
-        printf("%d", i);\n
+        setCodeVal(`#include <stdio.h>
+
+int main() {
+    int num = 5;
+    printf("Číslo: %d", num);
+
+    for (int i = 1; i <= 5; i++) {
+        printf("Iterace %d", i);
+    }
+
+    int result = 1;
+    for (int i = 1; i <= 5; i++) {
+        result *= i;
+    }
+    printf("Faktoriál z čísla 5 je %d", result);
+
+    return 0;
 }
 `)
     }, [])
 
     return (
         <div>
+            <title>Purkiáda bug hunt</title>
             <h1>Pomoz zpravit syntax errory po rekrutovi.</h1>
             <p>Protip: Compiler má vždy pravdu</p>
-            <textarea onChange={e => setCodeVal(e.target.value)} spellCheck={false} value={codeVal} name="" id="" cols={50} rows={15}>
-
-            </textarea>
+            <div className="errorLog">
+                <h2>Errory:</h2>
+                <ul>
+                    <li>Každý příkaz musí končit středníkem</li>
+                </ul>
+            </div>
+            <textarea onChange={e => setCodeVal(e.target.value)} spellCheck={false} value={codeVal} name="" id="" cols={50} rows={20} />
         </div>
     )
 }
