69cdee02f788bb2846f384096c4e008c20e09d6a
divear <lukyn.odehnal333@gmail.com>
2025-01-31 22:25:15 +0100

    changing trophy

--- pages/win.tsx
+++ pages/win.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
-import React from 'react'
+import React, {useState} from 'react'
 import trophy from "../components/imgs/trophy.jpg"
+import trophy2 from "../components/imgs/trophy2.jpg"
 
 function Win() {
+  const [isHover, setIsHover] = useState(false);
     return (
         <div className='intro'>
-            <Image className='winImg' src={trophy} alt="trofie"  />
+            <Image onMouseOver={()=>setIsHover(true)} onMouseOut={()=>setIsHover(false)} className='winImg' src={isHover ? trophy2 : trophy} alt="trofie"  />
 
             <h1 className='winText' >Vyhrál jsi</h1>
             <p>Máš maximální počet bodů z této úlohy, hodně štěstí s ostatními.</p>
